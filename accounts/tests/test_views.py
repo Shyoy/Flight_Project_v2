7ff7d2a7008097f7f874884c9ef92150626fb2c6
Shyoy <86570990+Shyoy@users.noreply.github.com>
2022-11-19 14:05:31 +0200

    Update test_views.py

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -5,16 +5,16 @@ from .mixins import TestDataMixin
 # Create your tests here.
 
 
-class CustomerProfileTest(TestDataMixin, TestCase):
+# class CustomerProfileTest(TestDataMixin, TestCase):
 
-    @classmethod
-    def setUpTestData(self):
-        super(CustomerProfileTest, self).setUpTestData
+#     @classmethod
+#     def setUpTestData(self):
+#         super(CustomerProfileTest, self).setUpTestData
 
-    def setUp(self):
-        pass
+#     def setUp(self):
+#         pass
 
-    def test_response(self):
-        pass
-        # response = self.client.get(reverse('homepage'))
-        # self.assertEqual(response.status_code, 200)
+#     def test_response(self):
+#         pass
+#         # response = self.client.get(reverse('homepage'))
+#         # self.assertEqual(response.status_code, 200)
