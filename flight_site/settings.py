f8c4364e6908a04a23fb4faa7067280f9c590855
Shyoy <86570990+Shyoy@users.noreply.github.com>
2022-12-21 19:17:28 +0200

    Update settings.py

--- flight_site/settings.py
+++ flight_site/settings.py
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     "crispy_bootstrap5",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
